c97d23cb2740a30f49c90c7b77993fad89219e5e
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-08-21 11:17:47 -0500

    First commit, project is empty

---
+++ src/main/java/com/meritameirca/banking/app/TeamFourMeritFinancialBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.meritameirca.banking.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFourMeritFinancialBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFourMeritFinancialBankingAppApplication.class, args);
+	}
+
+}
