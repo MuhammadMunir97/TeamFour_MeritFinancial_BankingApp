f06329344420c5e443adfde7285ca9a1337caba9
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-08-29 10:35:51 -0500

    service layer for the user

---
+++ src/main/java/com/meritameirca/banking/app/service/UserService.java
@@ -0,0 +1,40 @@
+package com.meritameirca.banking.app.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.meritameirca.banking.app.models.User;
+import com.meritameirca.banking.app.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	private final UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public List<User> findAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	public User findUserById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent()) {
+			return user.get();
+		}else {
+			return null;
+		}
+	}
+	
+	public void saveUser(User user) {
+		userRepository.save(user);
+	}
+	
+	public void deleteUserById(Long id) {
+		userRepository.deleteById(id);
+	}
+}
