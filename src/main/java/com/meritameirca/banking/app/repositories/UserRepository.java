36b1935d73ce2f420baa29e574ddffcac4999229
rsrob600 <47090183+rsrob600@users.noreply.github.com>
2019-08-29 11:28:34 -0500

    @Repository still missing from my latest pull for the users repo -- just added it again

--- src/main/java/com/meritameirca/banking/app/repositories/UserRepository.java
+++ src/main/java/com/meritameirca/banking/app/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.meritameirca.banking.app.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.meritameirca.banking.app.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 	List<User> findAll();
 }
